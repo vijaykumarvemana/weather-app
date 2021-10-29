5fa1ea960e2159b5a8c5cef4d70bf95c171fd1a8
vijay kumar vemana <vijaykumarvemana18@gmail.com>
2021-10-29 17:02:39 +0200

    Adds localstorage, auth

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,36 @@
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import mainReducer from '../reducers'
+import { persistStore, persistReducer } from 'redux-persist'
+import localStorage from 'redux-persist/lib/storage'
+import { encryptTransform } from 'redux-persist-transform-encrypt'
+import thunk from 'redux-thunk'
 
 
 export const initialState = {
   cityWeather: null
 }
 
-const configureStore = createStore(
-  mainReducer,
-  initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const persistConfig = {
+    key: 'root',
+    storage: localStorage,
+    transforms: [
+      encryptTransform({
+        secretKey: process.env.REACT_APP_SECRET_KEY,
+        onError: (error) => {
+          console.log(error)
+        },
+      }),
+    ],
+  }
+  const persistedReducer = persistReducer(persistConfig, mainReducer)
 
-export default configureStore
+const configureStore = createStore(
+  persistedReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(thunk))
+)
+const persistor = persistStore(configureStore)
+
+
+export  {configureStore, persistor}
